a9861e27101c4d9baf04db51303fc9a1c55bc2af
teddy@dnc-algo.com <lazebnik.teddy@gmail.com>
2020-08-15 19:45:19 +0300

    clear and put first idea of json managed data of website

--- js/main.js
+++ js/main.js
@@ -1,50 +1,222 @@
+// add header and footer to each page
 
-/* consts */
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
-const items = document.querySelectorAll(".item");
-/* end - consts */
-
-/*** run on page load ***/
-
-/* Event Listeners */
-for (let item of items)
+// init all the objects needed //
+let client;
+let thisPage = location.href.split("/").slice(-1)[0];
+if (thisPage == "")
 {
-	if (item.querySelector(".submenu")) 
-	{
-		item.addEventListener("click", toggleItem, false);
-		item.addEventListener("keypress", toggleItem, false);
-	}   
+	thisPage = "index";
 }
+// end - init all the objects needed //
 
-/*** end - run on page load ***/
+onPageLoad();
 
- 
-/* Toggle mobile menu */
-function toggleMenu() 
+function onPageLoad()
 {
-    if (menu.classList.contains("active")) 
+	// code for IE7+, Firefox, Chrome, Opera, Safari
+	if (window.XMLHttpRequest)
 	{
-		menu.classList.remove("active");
-        // adds the menu (hamburger) icon
-        toggle.querySelector("a").innerHTML = '<i class="fas fa-bars"></i>';
-    } 
-	else 
+		client = new XMLHttpRequest();
+	}
+	else // code for IE6, IE5
 	{
-        menu.classList.add("active");     
-        // adds the close (x) icon
-        toggle.querySelector("a").innerHTML = '<i class="fas fa-times"></i>';
-    }
+		client = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	// perform all the preparation actions needed 
+	loadHeader();
+	loadFooter();
+	loadSEO();
+	activeMenuLink();
+	loadNotificationPanel();
 }
- 
-/* Activate Submenu */
-function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
-  } else {
-    this.classList.add("submenu-active");
-  }
+
+// load the HTML of the header from the right file and put in the right location
+function loadHeader()
+{		
+	client.onreadystatechange  = htmlCodeHandler;
+	client.open("GET", "/components/header.html", false);
+	client.send();
 }
+
+// load the HTML of the header from the right file and put in the right location
+function loadFooter()
+{		
+	client.onreadystatechange  = htmlCodeHandler;
+	client.open("GET", "/components/footer.html", false);
+	client.send();
+}
+
+function htmlCodeHandler() 
+{
+	if (this.readyState == 4 && this.status == 200 && this.responseText != null)
+	{
+		document.getElementById("header").innerHTML = this.responseText;
+	}
+}
+
+// load the SEO data from JSON file and insert in the right locations
+function loadSEO()
+{		
+	client.onreadystatechange  = seoHandler;
+	client.open("GET", "/data/jsons/seo/" + thisPage + ".json", false);
+	client.send();
+}
+
+function seoHandler() 
+{
+	if (this.readyState == 4 && this.status == 200 && this.responseText != null)
+	{
+		var seoJsonObj = JSON.parse(this.responseText);
+		// run over all the keys
+		for (var key in seoJsonObj)
+		{
+			var content = "";
+			if (Array.isArray(seoJsonObj[key])) // if list
+			{
+				for (var key2 in seoJsonObj[key])
+				{
+					content += seoJsonObj[key][key2] + ",";
+				}
+				content = content.substring(0, content.length - 1);
+			}
+			else if (typeof seoJsonObj[key] == 'string') // if string
+			{
+				content = seoJsonObj[key];
+			}
+			else  // if different object
+			{
+				content = JSON.stringify(seoJsonObj[key]);
+			}
+			
+			// try to add it to the page
+			try
+			{
+				document.querySelector('meta[name="' + key + '"]').setAttribute("content", content);
+			}
+			catch (error)
+			{
+				try
+				{
+					document.getElementById(key).innerHTML = content;	
+				}
+				catch (error)
+				{
+					console.log("Error in 'seoHandler' while trying to add key = '" + key + "', because: " + error);
+				}
+			}
+		}
+	}
+}
+
+// mark the right menu link as active
+function activeMenuLink()
+{
+	$('.menu a').each(function() 
+	{
+		if ($(this).attr('id') == thisPage)
+		{
+			$(this).addClass('active'); 
+		}
+	});
+}
+
+
+/* notification panel on mian pages */
+function loadNotificationPanel()
+{	
+	// check if needed 
+	if (document.getElementById("notification-panel") == null)
+	{
+		return;
+	}
+	
+	client.onreadystatechange  = notificationHandler;
+	client.open("GET", "/notifications.txt", false);
+	client.send();
+}
+
+function notificationHandler() 
+{
+	if (this.readyState == 4)
+	{
+		if(this.status == 200 && this.responseText != null )
+		{
+			buildNotificationUI(this.responseText.split("\n"), true);
+		} 
+		else 
+		{
+			buildNotificationUI(this.status, false);
+		}
+	}
+}
+
+function buildNotificationUI(notifications, is_ok)
+{
+	var notfi_panel = document.getElementById("notification-panel");
+	var notfi_html = '<div class="notification-panel">';
+	// set notifications or error message
+	if (is_ok)
+	{
+		// build the panel
+		for (var i = 0; i < notifications.length; i++)
+		{
+			notfi_html += '<div class="notification"><p>' + notifications[i].trim().replace("script", "") + '</p></div>'; // the replace is to avoid JS injection in the original file
+		}
+	}
+	else
+	{
+		notfi_html += "<p> Error with status " + notifications + " while trying to retrive notifications - please inform the owner of the site regarding this error... </p>";
+	}
+	// set the content into the panel
+	notfi_html += '</div>';
+	notfi_panel.innerHTML = notfi_html;
+}
+/* end - notification panel on mian pages */
+
+/* help functions */
+
+function loadJSON(callback) 
+{   
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'my_data.json', true); // Replace 'my_data' with the path to your file
+	xobj.onreadystatechange = function () 
+	{
+		if (xobj.readyState == 4 && xobj.status == "200") 
+		{
+			// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+			callback(xobj.responseText);
+		}
+	};
+	xobj.send(null);  
+}
+
+function setCookie(cname, cvalue, exdays)
+{
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 8640000)); // 24 * 60 * 60 * 1000 = 8640000
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname)
+{
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) 
+	{
+		var c = ca[i];
+		while (c.charAt(0) == ' ') 
+		{
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) 
+		{
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+/* end - help functions */
