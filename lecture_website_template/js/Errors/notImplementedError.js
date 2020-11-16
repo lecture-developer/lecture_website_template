7dd467e5fd486ca6b1d295905d5f2688b2b523b2
Teddy Lazebnik <lazebnik.teddy@gmail.com>
2020-11-16 10:45:52 +0200

    fix pathes

--- lecture_website_template/js/Errors/notImplementedError.js
+++
@@ -1,9 +0,0 @@
-class NotImplemented extends Error
-{
-	constructor(message = "", ...args) 
-	{
-		super(message, ...args);
-		this.message = message + " has not yet been implemented.";
-		this.name = 'NotImplementedError';
-	}
-}
